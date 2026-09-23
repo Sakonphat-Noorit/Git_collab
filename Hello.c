cbb1b1255c712ee57488e589c7ac0df108b5ca91
69011857 <69011857@kmitl.ac.th>
2026-09-23 14:46:02 +0700

    add feature/farewell

--- Hello.c
+++ Hello.c
@@ -8,6 +8,12 @@ int greeting(const char *name)
 
 int main(void)
 {
-    greeting("Developer A,B,C !!");
-    return 0;
+greeting(“Developer A,B,C !!”);
+farewell(“Developer A,B,C”);
+return 0;
+}
+int farewell(const char *name)
+{
+printf("Goodbye, %s!\n", name);
+return 0;
 }
